2b75c4c3157ce7d5631921130b5825727cd507be
Ioczorozelaznog <141183674+Ignacio3108@users.noreply.github.com>
2025-04-24 16:10:21 -0300

    Add files via upload

---
+++ 2_CondoriLopez_M/0_pc2_8/0_pc2_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_pc2_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("primer numero que vas a dividir: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("segundo numero con el que vas a dividir: ");
+            int numero2 = int.Parse(Console.ReadLine());
+            Console.Write("el resto de la division es: ");
+            int resultado = numero1 % numero2;
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2_CondoriLopez_M/0_pc2_9/0_pc2_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_pc2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese radio: ");
+            double radio = double.Parse(Console.ReadLine());
+            double superficie = 4 * 3.14 * Math.Pow(radio, 2);
+            double volumen = 4 / 3 * 3.14 * Math.Pow(radio, 3);
+            Console.WriteLine("Superficie: " + superficie + ". Volumen: " + volumen + ".");
+            Console.ReadKey();
+
+        }
+    }
+}
