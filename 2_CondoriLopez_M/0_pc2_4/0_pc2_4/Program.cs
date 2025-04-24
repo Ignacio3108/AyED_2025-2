705b019876c9f257997dcbcb0c6acf1b3183f126
Ioczorozelaznog <141183674+Ignacio3108@users.noreply.github.com>
2025-04-24 16:09:09 -0300

    Add files via upload

---
+++ 2_CondoriLopez_M/0_pc2_4/0_pc2_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_pc2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Escribe un programa en C# que 
+            calcule el resultado de sumar 
+            dos números introducidos por el usuario.
+            */
+
+            Console.Write("Dame un numero: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Dame otro numero: ");
+            int numero2 = int.Parse(Console.ReadLine());
+            int resultado = numero1 + numero2;
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2_CondoriLopez_M/0_pc2_6/0_pc2_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_pc2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dame un numero: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Dame otro numero: ");
+            int numero2 = int.Parse(Console.ReadLine());
+            int resultado = numero1 * numero2;
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
