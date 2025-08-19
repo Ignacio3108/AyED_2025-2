cfd472dda7b86b07ed2cf2cd48fbd2773bea2dcb
Ioczorozelaznog <141183674+Ignacio3108@users.noreply.github.com>
2025-08-19 16:47:42 -0300

    etapa 3 - proy. 0,1,2

---
+++ 2_GonzalezOrozco_J/3_PC2_0/3_PC2_0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void hello_world (string nombre)
+{
+    Console.WriteLine("Hello, "+nombre+"!");
+}
+
+hello_world("raul alfonsin");
---
+++ 2_GonzalezOrozco_J/3_PC2_1/3_PC2_1/Program.cs
@@ -0,0 +1,7 @@
+﻿static int sumatoria(int v1, int v2)
+{
+    int calc = v1 + v2;
+    return calc;
+}
+
+Console.WriteLine(sumatoria(2, 2));
