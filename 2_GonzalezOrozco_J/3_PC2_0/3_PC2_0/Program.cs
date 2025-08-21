6d221fc9c5b5eb9990a42d34f447f1727b90ebf3
Ioczorozelaznog <141183674+Ignacio3108@users.noreply.github.com>
2025-08-21 15:11:27 -0300

    Delete 2_GonzalezOrozco_J/3_PC2_0 directory

--- 2_GonzalezOrozco_J/3_PC2_0/3_PC2_0/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-static void hello_world (string nombre)
-{
-    Console.WriteLine("Hello, "+nombre+"!");
-}
-
-hello_world("raul alfonsin");
